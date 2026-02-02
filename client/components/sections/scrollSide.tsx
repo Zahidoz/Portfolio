e1e1f38bd48948807872780f7cd9c8d39f39db36
Zahid <zahid.vahabzade@gmail.com>
2026-02-02 22:31:30 +0400

    [P-002] Layout created and sticky section almost done

---
+++ client/components/sections/scrollSide.tsx
@@ -0,0 +1,8 @@
+
+export default function ScrollSide() {
+  return (
+    <div className="flex-1">
+      Scroll
+    </div>
+  )
+}
---
+++ client/components/sections/stickySide.tsx
@@ -0,0 +1,12 @@
+
+export default function StickySide() {
+  return (
+    <div className="flex-1 w-full min-h-full bg-primary text-secondary flex flex-col gap-11 items-center justify-center">
+      <h1 className="text-7xl text-center">Zahid Vahabzada</h1>
+      <div className="w-[295px]  h-[450px]">
+        <img className="w-full h-full object-cover" src="me.jpeg" alt="" />
+      </div>
+      <h2 className="text-4xl text-center leading-tight">Senior Software Developer and <br /> Project Manager</h2>
+    </div>
+  )
+}
